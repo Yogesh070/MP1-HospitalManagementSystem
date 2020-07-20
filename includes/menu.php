c9c2b0e9eb08f6b767d805910b8ddd97c423526b
Yogesh070 <bese619@gces.edu.np>
2020-07-20 14:55:17 +0545

    menu file added

---
+++ includes/menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2 menu-items">
+                <nav class="nav flex-column pt-5">
+                    <a class="nav-link btn btn-primary active" href="index.php"><i class="fas fa-home"></i>Dashboard</a>
+                    <a class="nav-link btn patient" data-toggle="collapse" data-target="#Patient-menu" href="patient.php"><i class="fas fa-hospital-user"></i>Patient<i class="fas fa-chevron-down"></i></a>
+                    <div id="Patient-menu" class="collapse">
+                        <div>
+                            <a class="nav-link btn" href="patient.php"><i class="fas fa-chevron-right"></i>Patient Listing</a>
+                            <a class="nav-link btn" href="#"><i class="fas fa-chevron-right"></i>Assestment</a>
+                        </div>
+                    </div>
+                    <a class="nav-link btn" href="patient.php"><i class="fas fa-exchange-alt"></i>Shift</a>
+                    <a class="nav-link btn" href="#"><i class="fas fa-vial"></i>Lab</a>
+                    <a class="nav-link btn" href="#"><i class="fas fa-capsules"></i>Medication</a>
+                    <a class="nav-link btn" href="#"><i class="fas fa-calendar-check"></i>Appointment</a>
+                    <a class="nav-link btn" href="#"><i class="fas fa-cog"></i>Setting</a>
+                </nav>
+            </div>
