607fcbf619f2a95efac08e85ea1e3c880fe39b42
Yogesh070 <bese619@gces.edu.np>
2020-07-24 04:24:24 +0545

    Added update script

--- includes/addNewPatient.php
+++ includes/addNewPatient.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+$name='';
+$gender='';
+$dob='';
+$update=false;
+$id=0;
 
 $mysqli=new mysqli('localhost','root','','patientinfo') or die(mysqli_error($mysqli));
 
@@ -9,7 +14,7 @@ if(isset($_POST['save'])){
     $dob = $_POST['dob'];
     $mysqli->query("INSERT INTO patientdata (Name, Gender, DOB) VALUES ('$name','$gender','$dob')") or die($mysqli->error);
 
-    $_SESSION['message']="Record has been updated!";
+    $_SESSION['message']="Record has been added Successfully!";
     $_SESSION['msg_type']="success";
 
     header("location: ../patient.php");
@@ -19,8 +24,33 @@ if(isset($_GET['delete'])){
     $id=$_GET['delete'];
     $mysqli->query("DELETE FROM patientdata WHERE ID=$id") or die($mysqli->error);
 
-    $_SESSION['message']="Record has been deleted";
+    $_SESSION['message']="Record has been deleted Successfully!";
     $_SESSION['msg_type']="danger";
 
     header("location: ../patient.php");
+}
+if(isset($_GET['edit'])){
+    $id=$_GET["edit"];
+    $update=true;
+    $result=$mysqli->query("SELECT * FROM patientdata WHERE ID=$id") or die($mysqli->error);
+    if(count($result)==1){
+        $row=$result->fetch_array();
+        $name=$row['Name'];
+        $gender=$row['Gender'];
+        $dob=$row['DOB'];
+    }
+}
+
+if(isset($_POST['update'])){
+    $id=$_POST['id'];
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $dob = $_POST['dob'];
+    $mysqli->query("UPDATE patientdata SET Name='$name', Gender='$gender', DOB='$dob' WHERE ID=$id") or die($mysqli->error);
+
+    $_SESSION['message']="Record has been updated Successfully";
+    $_SESSION['msg_type']="warning";
+
+    header("location: ../patient.php");
+
 }
