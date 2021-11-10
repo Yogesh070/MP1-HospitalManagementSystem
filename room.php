209b6ba11167323c335571973034efc937420731
Yogesh070 <ythapa430@gmail.com>
2021-11-10 19:12:12 +0545

    rooms page

---
+++ room.php
@@ -0,0 +1,34 @@
+<?php include "includes/header.php"?>
+<?php include "includes/menu.php"?>
+<div class="col-md-10 " >
+    <h2 class="h1 mt-5" style="font-weight:bold"> Room Status</h2>
+    <table class="table mt-3"  style="text-align:center">
+    <thead class="thead-dark">
+        <tr>
+        <th scope="col">Room Id</th>
+        <th scope="col">Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Available</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Available</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td>Not Available</td>
+        </tr>
+        <tr>
+        <th scope="row">4</th>
+        <td> Available</td>
+        </tr>  <tr>
+        <th scope="row">5</th>
+        <td>Not Available</td>
+        </tr>
+    </tbody>
+    </table>
+</div>
